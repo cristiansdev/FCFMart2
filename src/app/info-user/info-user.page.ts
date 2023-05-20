d653e236d6b679d7b86ab77937d7320d9b1708eb
cristian <cristian192003@outlook.es>
2023-05-20 10:45:07 -0600

    Se cambio la ventana de info usuario

--- src/app/info-user/info-user.page.ts
+++ src/app/info-user/info-user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vendedor } from '../interfaces/vendedor';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
   selector: 'app-info-user',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-user.page.scss'],
 })
 export class InfoUserPage implements OnInit {
-
-  constructor() { }
+  public vendedor: Vendedor = {
+    nombre: '',
+    password: '',
+    correo: '',
+    slogan: '',
+    desripcion: '',
+  };
+  idUser: string;
+  constructor(private productoService: ProductoService) {
+    let n = localStorage.getItem('idUser');
+    if (n != null) {
+      this.idUser = n;
+    } else {
+      this.idUser = '';
+    }
+   }
 
   ngOnInit() {
   }
-
+  obtenerVendedor() {
+    if (this.idUser != null) {
+      this.productoService.getVendedor(this.idUser).subscribe((res) => {
+        this.vendedor = res;
+      });
+    }
+  }
 }
